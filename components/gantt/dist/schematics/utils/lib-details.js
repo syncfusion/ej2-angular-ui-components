23c67f6809098e21322ac92234c4b7fc90013538
pipeline <pipeline@syncfusion.com>
2019-12-03 12:34:05 +0000

    v17.3.30 is released

--- components/gantt/dist/schematics/utils/lib-details.js
+++ components/gantt/dist/schematics/utils/lib-details.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pkgName = '@syncfusion/ej2-angular-gantt';
-exports.pkgVer = '^17.3.28';
+exports.pkgVer = '^17.3.29';
 exports.moduleName = 'GanttModule';
-exports.themeVer = '~17.3.28';
+exports.themeVer = '~17.3.29';
